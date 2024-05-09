8ab6778e9a0ba9aaa4e31c816f45aaf7074d3032
David Schurer <david.schurer@yahoo.com>
2024-05-09 12:12:52 -0700

    added home page popup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,20 @@ import styles from "./page.module.scss";
 export default function Home() {
     return (
         <main className={styles.main}>
-            <div>
-                <h1>U-Cook</h1>
-                <p>Welcome to U-Cook!</p>
-                <p>LETS GET COOKING 🧑‍🍳</p>
+            <div className={styles.UCookHomePage}>
+              <div className={styles.WelcomePopUp}>
+                <p>Welcome to the U-Cook Virtual Cooking Assistant! <br/><br/>
+                  This application strives to help you learn to cook with friends and family while using voice recognition to <br/>
+                  effortlessly browse through different recipes and cookbooks! <br/><br/>
+                  To start cooking please press continue!
+                </p>
+
+                <button type="submit"><strong>Continue</strong></button>
+              </div>
+             
             </div>
         </main>
+
+  
     );
 }
