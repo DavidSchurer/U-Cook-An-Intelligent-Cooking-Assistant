44e0b8d04f54d9382e048148d373d4bdbea58edf
David Schurer <david.schurer@yahoo.com>
2024-05-10 23:57:57 -0700

    update

--- src/app/contacts-page/page.tsx
+++ src/app/contacts-page/page.tsx
@@ -1,9 +1,21 @@
-import React from "react";
+'use client'
+import React from 'react';
+import Image from "next/image";
+import styles from "./page.module.scss";
+import { useInput } from 'compo/global/context/InputContext';
+import { useRouter } from "next/navigation";
 
 export default function ContactsPage() {
     return (
-        <div>
-            
-        </div>
+            <div className={styles.ContactsPage}>
+                <h1>Please Choose from your list of contacts to create group call:</h1>
+                <ul>
+                    <li>David</li>
+                    <li>Ben</li>
+                    <li>Shivam</li>
+                    <li>Harshitha</li>
+                </ul>
+                <button type="submit"><strong>Continue</strong></button>
+            </div>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,6 @@ import { useRouter } from "next/navigation";
 
 export default function Home() {
   const router = useRouter();
-
   const input = useInput();
 
     const handleContinueClick = () => {
