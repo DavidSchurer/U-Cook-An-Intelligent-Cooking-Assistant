f4b159c3f79ba178b0b243c2fd88fa31a55a30e0
Hershey-12 <73355095+Hershey-12@users.noreply.github.com>
2024-05-12 00:53:29 -0700

    Added css and images
    Added css and images to the categories page

--- src/app/categories-page/page.tsx
+++ src/app/categories-page/page.tsx
@@ -4,36 +4,50 @@ import Image from "next/image";
 import styles from "./page.module.scss";
 import { useInput } from 'compo/global/context/InputContext';
 import { useRouter } from "next/navigation";
+import pastaImage from './pasta.jpg';
+import soupImage from './soups.jpg';
+import sandwichesImage from './sandwiches.jpeg';
+import chickenImage from './chicken.jpg';
 
 export default function CategoriesPage() {
     return (
             <div className={styles.CategoriesPage}>
                 <h1>Please choose a recepie category:</h1>
-                <ul>
-                    <li className = "horizotal-list">
-                        <button class="button" onclick="">
-                            <img src = "" alt ="pasta"/>
+                <ul className = {styles ["horizontal-list"]}>
+                    <li className= {styles ["horizontal-list-item"]}>
+                        <button className="button" >
+                            <div className="Image">
+                            <Image src = {pastaImage}
+                             height= {150}
+                             alt ="pasta"/>
+                            </div>
                             <span className = "button-text">Pastas</span>
                         </button>
                     </li>
                         
-                    <li className = "horizotal-list">
-                        <button class="button" onclick="">
-                            <img src = "" alt ="Sandwiches"/>
+                    <li className={styles ["horizontal-list-item"]}>
+                        <button className="button" >
+                        <div className="Image">
+                        <Image height= {150} src = {sandwichesImage} alt ="sandwiches"/>
+                        </div>
                             <span className = "button-text">Sandwiches</span>
                         </button>
                     </li>
                     
-                    <li className = "horizotal-list">
-                        <button class="button" onclick="">
-                            <img src = "" alt ="Soups"/>
+                    <li className={styles ["horizontal-list-item"]}>
+                        <button className="button" >
+                            <div className="Image">
+                                <Image src = {soupImage} height= {150} alt ="Soups"/>
+                            </div>
                             <span className = "button-text">Soups</span>
                         </button>
                     </li>
 
-                    <li className = "horizotal-list">
-                        <button class="button" onclick="">
-                            <img src = "" alt ="Chicken"/>
+                    <li className={styles ["horizontal-list-item"]}>
+                        <button className="button" >
+                        <div className="Image">
+                        <Image src = {chickenImage} height= {150} alt ="chicken"/>
+                        </div>
                             <span className = "button-text">Chicken</span>
                         </button>
                     </li>
