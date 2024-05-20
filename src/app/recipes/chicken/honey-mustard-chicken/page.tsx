92025a7151eabe09e25a8cd876d55ca0d30ff9f9
Chameebling <chameebling518@gmail.com>
2024-05-19 22:41:24 -0700

    added return call screen voice commands

--- src/app/recipes/chicken/honey-mustard-chicken/page.tsx
+++ src/app/recipes/chicken/honey-mustard-chicken/page.tsx
@@ -1,10 +1,12 @@
 'use client'
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import styles from './page.module.scss';  // Ensure you have the corresponding CSS module for styles
+import { useInput } from 'compo/global/context/InputContext';
 
 export default function HoneyMustardChicken() {
     const router = useRouter();
+    const { addVoiceRoute, removeVoiceRoute } = useInput();
 
     // Function to handle navigation to the detailed recipe page
     const handleSelectRecipe = (id: string) => {
@@ -15,6 +17,18 @@ export default function HoneyMustardChicken() {
     const handleCallScreenButton = () => {
         router.push(`/call-screen`);
     }
+
+    useEffect(() => {
+        // Add voice route for selecting this recipe
+        addVoiceRoute('call screen', 'Okay, we are back to the call screen.', handleCallScreenButton, {
+            visual: 'Return to Call Screen'
+        });
+
+        return () => {
+            // Remove voice route when component is unmounted
+            removeVoiceRoute('call screen');
+        }
+    }, []);
 
     return (
         <>
