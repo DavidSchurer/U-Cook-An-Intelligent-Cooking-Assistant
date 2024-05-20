6c018b6addcdb2320bf20a53e55d213da5e06b27
Chameebling <chameebling518@gmail.com>
2024-05-19 20:53:27 -0700

    prepared call screen

---
+++ src/app/call-screen/page.tsx
@@ -0,0 +1,9 @@
+import styles from './page.module.scss';
+
+export default function CallScreen() {
+    return (
+        <div className={styles.container}>
+            <h1>Call Screen</h1>
+        </div>
+    )
+}
