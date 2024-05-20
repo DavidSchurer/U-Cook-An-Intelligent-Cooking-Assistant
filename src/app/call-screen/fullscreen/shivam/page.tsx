42b85aa8c37de4d2fc25b8289cb550e4126fcffd
Chameebling <chameebling518@gmail.com>
2024-05-19 21:00:20 -0700

    fullscreen options for group call

---
+++ src/app/call-screen/fullscreen/shivam/page.tsx
@@ -0,0 +1,9 @@
+import styles from '../subpages.module.scss';
+
+export default function BenFullScreen() {
+    return (
+        <div className={styles.container}>
+            <h1>Ben Full Screen</h1>
+        </div>
+    )
+}
