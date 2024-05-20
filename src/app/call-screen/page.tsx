27ffdb71341331b82c75ed2445b9720535266472
David Schurer <david.schurer@yahoo.com>
2024-05-19 22:11:45 -0700

    update

--- src/app/call-screen/page.tsx
+++ src/app/call-screen/page.tsx
@@ -1,5 +1,8 @@
+'use client'
 import Image from 'next/image';
 import styles from './page.module.scss';
+import { useRouter } from "next/navigation";
+import React, { useEffect } from 'react';
 
 interface ParticipantTileProps {
     name: string;
@@ -18,21 +21,36 @@ function ParticipantTile({name, image}: ParticipantTileProps){
 }
 
 function BottomPanel(){
+    const router = useRouter();
+
+    const handleButtonClick = () => {
+        router.push('/categories-page');
+    };
+
     return (
         <div className={styles.bottomPanel}>
             <div className={styles.bottomPanelItem}>
                 <button className={styles.bottomPanelButton}>
                     <Image src="/icons/microphone.svg" alt="Microphone" width={24} height={24} />
+                    <span>Microphone</span>
                 </button>
             </div>
             <div className={styles.bottomPanelItem}>
                 <button className={styles.bottomPanelButton}>
                     <Image src="/icons/video-camera.svg" alt="Video Camera" width={24} height={24} />
+                    <span>Video Camera</span>
                 </button>
             </div>
             <div className={styles.bottomPanelItem}>
                 <button className={styles.bottomPanelButton}>
                     <Image src="/icons/end-call.svg" alt="End Call" width={24} height={24} />
+                    <span>End Call</span>
+                </button>
+            </div>
+            <div className={styles.bottomPanelItem} onClick={handleButtonClick}>
+                <button className={styles.bottomPanelButton}>
+                    <Image src="/icons/recipe-categories.svg" alt="Recipe Categories" width={24} height={24} />
+                    <span>Recipe Categories</span>
                 </button>
             </div>
         </div>
